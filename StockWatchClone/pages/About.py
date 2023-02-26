098bac4bba064e37275be31b59f6cccc154c7472
Oscar <iu22229@bristol.ac.uk>
2023-02-26 21:39:44 +0000

    Addition

--- StockWatchClone/pages/About.py
+++ StockWatchClone/pages/About.py
@@ -13,14 +13,14 @@ def about():
     st.markdown('<div style="text-align: center;">StockWatch is a comprehensive dashboard that enables you to '
                 'effortlessly track national-level and individual supermarket stock for various products.</div>',
                 unsafe_allow_html=True)
-    with open("PresentationStockWatch.pdf", "rb") as f:
-        pdf_bytes = f.read()
-        pdf_base64 = b64encode(pdf_bytes).decode('utf-8')
-        pdf_embed = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="300" ' \
-                    f' type="application/pdf"></iframe>'
-    st.subheader("Brishack 2023 - Team StockWatch")
-
-    st.markdown(pdf_embed, unsafe_allow_html=True)
+    # with open("PresentationStockWatch.pdf", "rb") as f:
+    #     pdf_bytes = f.read()
+    #     pdf_base64 = b64encode(pdf_bytes).decode('utf-8')
+    #     pdf_embed = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="300" ' \
+    #                 f' type="application/pdf"></iframe>'
+    # st.subheader("Brishack 2023 - Team StockWatch")
+    #
+    # st.markdown(pdf_embed, unsafe_allow_html=True)
 
 
 def main():
