aaaf80c90137926ccb879d655e71ca88c2b4601e
1uked <123551653+1uked@users.noreply.github.com>
2023-02-26 12:07:55 +0000

    example streamline

--- main.py
+++ main.py
@@ -1,23 +1,54 @@
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
+import streamlit as st
+import pandas as pd
+import sqlite3
+conn = sqlite3.connect('data/Chinook_Sqlite.sqlite')
+c = conn.cursor()
+
+def sql_executor(raw_code):
+    c.execute(raw_code)
+    data = c.fetchall()
+    return data
+
+def main():
+    st.title("Lux")
+    menu = ["Home","Login","Create Account","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
+    if choice == "Home":
+        st.subheader("HomePage")
+        col1,col2,col3,col4,col5 = st.columns(5)
+        with col1:
+            with st.form(key='query_form'):
+                raw_code = st.text_area("Look up here")
+                submit_code = st.form_submit_button("Execute")
+            with st.expander("Table Info"):
+                t_info = {}
+
+        with col2:
+            if submit_code:
+                st.info("Query Submitted")
+                st.code(raw_code)
+                query_results = sql_executor(raw_code)
+                with st.expander("Results"):
+                    st.write(query_results)
+                with st.expander("Pretty Table"):
+                    query_df = pd.DataFrame(query_results)
+                    st.dataframe(query_df)
 
 
-def nth_prime(n):
-    if n <= 0:
-        return None
-    prime_counter = 0
-    num = 2
-    while prime_counter < n:
-        if is_prime(num):
-            prime_counter += 1
-        num += 1
-    return num - 1
+    elif choice == "About":
+        st.subheader("About")
+    elif choice == "Create Account":
+        st.subheader("Create Account")
+    elif choice == "Login":
+        st.subheader("Login")
 
 
-print(nth_prime(9))
-print(nth_prime(7))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
