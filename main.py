0cd824f03041735f62f12f307abdb74d38d18194
Oscar <iu22229@bristol.ac.uk>
2023-02-26 12:12:17 +0000

    Added addition

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
+
 conn = sqlite3.connect('data/Chinook_Sqlite.sqlite')
 c = conn.cursor()
+
 
 def sql_executor(raw_code):
     c.execute(raw_code)
     data = c.fetchall()
     return data
 
+
 def main():
     st.title("Lux")
-    menu = ["Home","Login","Create Account","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
+    menu = ["Home", "Login", "Create Account", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
     if choice == "Home":
         st.subheader("HomePage")
-        col1,col2,col3,col4,col5 = st.columns(5)
+        col1, col2, col3, col4, col5 = st.columns(5)
         with col1:
             with st.form(key='query_form'):
                 raw_code = st.text_area("Look up here")
@@ -34,7 +37,6 @@ def main():
                     query_df = pd.DataFrame(query_results)
                     st.dataframe(query_df)
 
-
     elif choice == "About":
         st.subheader("About")
     elif choice == "Create Account":
@@ -43,12 +45,5 @@ def main():
         st.subheader("Login")
 
 
-
-
-
-
-
 if __name__ == '__main__':
     main()
-
-
