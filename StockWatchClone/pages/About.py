4a6f023783a59c7b1ed10f45f4e2df247c8a8c0e
Yogadhveep <px22472@bristol.ac.uk>
2023-02-26 21:36:50 +0000

    Madness

--- StockWatchClone/pages/About.py
+++ StockWatchClone/pages/About.py
@@ -3,20 +3,22 @@ from base64 import b64encode
 import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>About</h1>", unsafe_allow_html=True)
-#st.markdown("<h1 style='text-align: center;'>Using StockWatch</h1>", unsafe_allow_html=True)
-st.markdown('<div style="text-align: center;">StockWatch is a comprehensive dashboard that enables you to effortlessly track national-level and individual supermarket stock for various products.</div>',unsafe_allow_html=True)
+# st.markdown("<h1 style='text-align: center;'>Using StockWatch</h1>", unsafe_allow_html=True)
+st.markdown(
+    '<div style="text-align: center;">StockWatch is a comprehensive dashboard that enables you to effortlessly track national-level and individual supermarket stock for various products.</div>',
+    unsafe_allow_html=True)
 
 
 def about():
     st.markdown("")
-    #st.subheader("<h1 style='text-align: center;'>Using StockWatch</h1>", unsafe_allow_html=True)
-    #st.markdown('<div style="text-align: center;">StockWatch is a comprehensive dashboard that enables you to effortlessly track national-level and individual supermarket stock for various products.</div>', unsafe_allow_html=True)
+    # st.subheader("<h1 style='text-align: center;'>Using StockWatch</h1>", unsafe_allow_html=True)
+    # st.markdown('<div style="text-align: center;">StockWatch is a comprehensive dashboard that enables you to effortlessly track national-level and individual supermarket stock for various products.</div>', unsafe_allow_html=True)
     # with open("PresentationStockWatch.pdf", "rb") as f:
     #     pdf_bytes = f.read()
     #     pdf_base64 = b64encode(pdf_bytes).decode('utf-8')
     #     pdf_embed = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="300" ' \
     #                 f' type="application/pdf"></iframe>'
-    #st.subheader("Brishack 2023 - Team StockWatch")
+    # st.subheader("Brishack 2023 - Team StockWatch")
     # with open("PresentationStockWatch.pdf", "rb") as f: pdf_bytes = f.read() pdf_base64 = b64encode(
     # pdf_bytes).decode('utf-8') pdf_embed = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700"
     # height="300" type="application/pdf"></iframe>'
