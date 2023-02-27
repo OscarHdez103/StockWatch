7b7811e4f7c811a73cf8f18ea6229b0076c9257e
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2023-02-27 01:20:12 +0000

    Table SQL

--- StockWatchClone/Backend.py
+++ StockWatchClone/Backend.py
@@ -5,7 +5,7 @@ c = conn.cursor()
 
 
 def total(product, supermarket):
-    c.execute("SELECT product_quantity FROM " + supermarket + " WHERE product_name = '" + product + "'")
+    c.execute("SELECT product_quantity FROM '" + supermarket + "' WHERE product_name = '" + product + "'")
     quantity = c.fetchall()
     c.execute("UPDATE Products SET quantity_total = quantity_total +  ? WHERE product_name = ?", (quantity[0][0], product.title()))
     conn.commit()
@@ -14,10 +14,12 @@ def total(product, supermarket):
 def all_products():
     c.execute("UPDATE Products SET quantity_total = 0")
     c.execute("SELECT product_name FROM Products")
-    names = c.fetchall()
-    for name in names:
-        for supermarket in ["Tesco", "Iceland", "Asda", "Morrisons", "`Co-op`"]:
-            total(name[0], supermarket)
+    products = c.fetchall()
+    c.execute("SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE 'Products';")
+    supermarkets = c.fetchall()
+    for supermarket in supermarkets:
+        for product in products:
+            total(product[0], supermarket[0])
 
 
 def main():
