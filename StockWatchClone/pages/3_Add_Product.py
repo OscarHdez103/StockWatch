4de86c5ef609fb4bd637892c76664d728963d853
1uked <123551653+1uked@users.noreply.github.com>
2023-02-27 01:21:35 +0000

    Login

--- StockWatchClone/pages/3_Add_Product.py
+++ StockWatchClone/pages/3_Add_Product.py
@@ -1,29 +1,92 @@
 import sqlite3
 import streamlit as st
 import streamlit_authenticator as stauth
+import pickle
+from pathlib import Path
 
-
-def login():
-    #st.markdown("<h1 style='text-align: center;'>Add Product</h1>", unsafe_allow_html=True)
-    names = ['John Smith', 'Rebecca Briggs']
-    usernames = ['jsmith', 'rbriggs']
-    passwords = ['123', '456']
-    hashed_passwords = stauth.hasher(passwords).generate()
-    authenticator = stauth.authenticate(names, usernames, hashed_passwords, 'cookie_name', '1234', cookie_expiry_days=30)
-    name, authentication_status = authenticator.login('Login', 'sidebar')
-    if authentication_status:
-        st.write("Welcome * % s *" % (name))
-        # your application
-    elif authentication_status == False:
-        st.error("Username / password is incorrect")
-    elif authentication_status == None:
-        st.warning("Please enter your username and password")
+names =["Peter Parker","Rebecca Miller"]
+usernames = ["pparker","rmiller"]
 
 
 
+file_path = Path(__file__).parent / "user_data/hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+credentials = {"usernames": {}}
+
+for uname, name, pwd in zip(usernames, names, hashed_passwords):
+    user_dict = {"name": name, "password": pwd}
+    credentials["usernames"].update({uname: user_dict})
+
+authenticator = stauth.Authenticate(credentials,"addition_dashboard","abc", cookie_expiry_days=30)
+name,authentication_status,username = authenticator.login("Login","main")
+
+if authentication_status == False:
+    st.error("Username/Password is incorrect")
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+if authentication_status:
+    st.write("Success")
+
+    #Logout
+    authenticator.logout("Logout","sidebar")
+    #st.sidebar.title(f"Welcome {name}")
 
 
 
 
-if __name__ == '__main__':
-    login()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# usernames = ['user1', 'user2']
+# names = ['name1', 'name2']
+# passwords = ['pwd1', 'pwd2']
+#
+# credentials = {"usernames": {}}
+#
+# for uname, name, pwd in zip(usernames, names, passwords):
+#     user_dict = {"name": name, "password": pwd}
+#     credentials["usernames"].update({uname: user_dict})
+#
+#
+# #st.markdown("<h1 style='text-align: center;'>Add Product</h1>", unsafe_allow_html=True)
+#
+#
+#
+#
+#
+#
+# def main(credentials):
+#     authenticator = stauth.Authenticate(credentials, "cookie_name", "random_key", cookie_expiry_days=30)
+#
+#     authentication_status = authenticator.login('Login', 'main')
+#
+#     if authentication_status:
+#         st.subheader("Welcome")
+#         # your application
+#     elif authentication_status == False:
+#         st.error("Username / password is incorrect")
+#     elif authentication_status == None:
+#         st.warning("Please enter your username and password")
+#
+#
+# if __name__ == '__main__':
+#     main(credentials)
